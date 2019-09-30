7c3ee0294eaebf67dd7492750431b50b9f02cdb6
Doug <me@douglasbowen.com>
2019-09-30 12:56:18 -0400

    arrays.py

---
+++ arrays/array.py
@@ -0,0 +1,8 @@
+# Things we need for an advanced array
+# size of it and protection from going past that
+# size of objects
+# to append need space
+# track allocated and used
+# if we run out of space, we need to make a new one with more space
+# then copy each item over
+
